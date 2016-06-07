b09208d99ee92875d8c17170eaafcb61962dd115
derek_robertson <dwilliamrobertson@gmail.com>
2016-06-07 15:54:34 -0700

    fixed timer and score

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,7 +2,7 @@ var menuState = {
 
   create: function(){
 
-    menuScroll = game.add.tileSprite(0, 0, 600, 1000, 'menuScroll');
+    menuScroll = game.add.tileSprite(0, 0, 600, 950, 'menuScroll');
 
     var nameLabel = game.add.text(80, 80, 'Project Brain Storm', {font: '50px Arial', fill: '#ffffff'});
 
@@ -14,7 +14,7 @@ var menuState = {
   },
 
   update: function(){
-    menuScroll.tilePosition.y += 2;
+    menuScroll.tilePosition.y += 1;
   },
 
   start: function(){
--- src/js/win.js
+++ src/js/win.js
@@ -4,6 +4,10 @@ var winState = {
 
     var winLabel = game.add.text(80, 80, 'You Won!', {font: '50px Arial', fill: '#00FF00' });
 
+    var scoreLabel = game.add.text(80, 80, 'Your ' + score, {font: '32px Arial', fill: '#00FF00' });
+
+    var bsLabel = game.add.text(80, 80, 'num of backspaces used: ' + bsCount, {font: '50px Arial', fill: '#00FF00' });
+
     var spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     spacebar.onDown.addOnce(this.restart, this);
